71975d9f922a93b9a1a5c1ebe7ba01538178248e
lala341 <li.forero@uniandes.edu.co>
2019-11-18 13:44:30 -0500

    Cambios

--- src/components/job.js
+++ src/components/job.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {FormattedDate,FormattedNumber, FormattedPlural} from 'react-intl';
+
 
 export default class Job extends React.Component {
 
@@ -8,9 +10,26 @@ export default class Job extends React.Component {
   				<th scope="row">{this.props.offer.id}</th>
       			<td>{this.props.offer.name}</td>
   				<td>{this.props.offer.company}</td>
-      			<td>{this.props.offer.salary}</td>
+      			<td>{this.props.offer.salary} 
+				  <FormattedPlural
+    value={this.props.offer.salary}
+	one=" milion"
+	other=" milions"
+  />
+				  
+				  
+				  </td>
       			<td>{this.props.offer.city}</td>
-      			<td>{this.props.offer.date}</td>
+      			<td>
+  <FormattedDate
+    value={new Date(this.props.offer.date)}
+    year='numeric'
+    month='long'
+    day='numeric'
+    weekday='long'
+  />
+</td>
+<td><FormattedNumber value={this.props.offer.views}/></td>
   			</tr>
   		);
 	}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
 
 import JobsList from "./components/jobsList";
+import localeEsMessages from "./locales/es";
 
-ReactDOM.render(<JobsList/>, document.getElementById("root"));
+import localeEnMessages from "./locales/en";
+
+let getBrowserLanguage=()=>{
+
+    var m=navigator.language|| navigator.userLanguage;
+console.log(m);
+return navigator.language|| navigator.userLanguage;
+}
+
+let getLanguage=()=>{
+
+    var u=getBrowserLanguage();
+
+    if(u==="es-ES"){
+        return "es";
+    }
+    return "en";
+}
+let getLanguageData=()=>{
+
+    var u=getBrowserLanguage();
+
+    if(u==="es-ES"){
+        return localeEsMessages;
+    }
+    return localeEnMessages;
+}
+
+ReactDOM.render(
+    
+	<IntlProvider locale={getLanguage()} messages={getLanguageData()}>
+		<JobsList/>
+	</IntlProvider>, document.getElementById("root")
+);
